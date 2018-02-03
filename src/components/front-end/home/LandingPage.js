9a0688f1889b1ce181db3cec5cf2a5c8fcaba7ba
ivangsp <talk2gspivan@outlook.com>
2018-02-03 19:24:48 +0200

    now searchbox is responsive now

--- src/components/front-end/home/LandingPage.js
+++ src/components/front-end/home/LandingPage.js
@@ -17,51 +17,52 @@ class LandingPage extends  Component {
         return(
             <div className="container-fluid landingpage-img">
                 <div className="row">
-                    <div className="col-md-3 offset-md-1 mt-5 col-xs-0 ">
-                       <div className="reservation-dialog">
-                           <form >
-                             <div className="form-row row">
-                                 <div className="col-md-6 col-xs-6">
-                                     <label><i className="fa fa-calendar-o"></i> Check-in</label>
-                                     <Datepicker startDate={moment()} />
-                                 </div>
-                                 <div className="col-md-6 col-xs-6">
-                                     <label><i className="fa fa-calendar-o"></i> Check-out</label>
-                                     <Datepicker startDate={moment().add(1, "days")} />
-                                 </div>
-                             </div>
+                    <div className="col-md-3 offset-md-1 mt-5  reservation-dialog ">
+                        <div className="form-row row">
+                            <div className="col-6">
+                                <label><i className="fa fa-calendar-o"></i> Check-in</label>
+                                <Datepicker startDate={moment()} />
+                            </div>
+                            <div className="col-6">
+                                <label><i className="fa fa-calendar-o"></i> Check-out</label>
+                                <Datepicker startDate={moment().add(1, "days")} />
+                            </div>
+                        </div>
 
-                               <div className="row mt-3 ">
-                                   <div className="col-md-2"><i className="fa fa-bed" aria-hidden="true"> </i></div>
-                                   <div className="col-md-4 "> <b className="">Rooms</b> </div>
-                                   <div className="col-md-6 col-sm-6" ><FormSelection  val={1}/></div>
-                               </div>
 
-                               <div className="form-row mt-3">
-                                   <div className="form-group col-md-6">
-                                       <label><i className="fa fa-male"></i> Adults</label>
-                                       <input type="number" value={1} className="form-control"  />
-                                   </div>
-                                   <div className="form-group col-md-6">
-                                       <label><i className="fa fa-child"></i> Kids</label>
-                                       <input type="number" value={0} className="form-control"  />
-                                   </div>
-                               </div>
+                        <div className="row form-row mt-3">
+                            <div className="form-group col-6">
+                                <label><i className="fa fa-male"></i> Adults</label>
+                                <input type="number" value={1} className="form-control"  />
+                            </div>
+                            <div className="form-group col-6">
+                                <label><i className="fa fa-child"></i> Kids</label>
+                                <input type="number" value={0} className="form-control"  />
+                            </div>
+                        </div>
 
-                               <div className="form-group col-md-12">
-                                   <label htmlFor="exampleInputPassword1" className="label">Coupon Code</label>
-                                   <input type="numeric" value="" className="form-control" id="exampleInputPassword1" />
-                               </div>
+                       <div className="row">
+                           <div className="col-6">
+                               <label> Rooms</label>
+                               <FormSelection  val={1}/>
+                           </div>
+                           <div className="form-group col-6">
+                               <label htmlFor="exampleInputPassword1" className="label">Coupon Code</label>
+                               <input type="numeric" value="" className="form-control" id="exampleInputPassword1" />
+                           </div>
 
-                               <Link to="/booking" type="submit" className="btn btn-danger btn-bgcolor col-md-12">
-                                   Search</Link>
-                           </form>
                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <Link to="/booking" type="submit" className="btn btn-danger btn-bgcolor col-md-12">
+                                    Search</Link>
+                            </div>
+                        </div>
                     </div>
-
                 </div>
 
             </div>
+
         );
     }
 }
